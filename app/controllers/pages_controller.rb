dc0e8b91bc3c4a37947ae67ccbfc8134412d8e88
mangochiman <mangochiman@gmail.com>
2016-10-12 03:35:37 +0200

    Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
     @page_title = "Home"
+    primary_category_id = Category.find_by_name("PRIMARY").category_id
+    secondary_category_id = Category.find_by_name("SECONDARY").category_id
+    tertiary_category_id = Category.find_by_name("TERTIARY").category_id
+
+    @primary_books = BookCategory.find(:all, :conditions => ["category_id =?", primary_category_id])
+    @secondary_books = BookCategory.find(:all, :conditions => ["category_id =?", secondary_category_id])
+    @tertiary_books = BookCategory.find(:all, :conditions => ["category_id =?", tertiary_category_id])
   end
 end
