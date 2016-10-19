64f6fc1961db0d29b48b6672132f167d3ef33b79
mangochiman <mangochiman@gmail.com>
2016-10-19 20:53:29 +0200

    Non Admin UI

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,32 @@
 class StudentsController < ApplicationController
+  layout 'students'
+  
   def dashboard
-    
-    render :layout => "students"
+
   end
+
+  def select_books_from_store
+
+  end
+
+  def update_my_book_shelf
+
+  end
+  
+  def remove_books_from_shelf_menu
+
+  end
+
+  def delete_books_from_menu
+
+  end
+  
+  def my_recent_books_menu
+
+  end
+
+  def extras
+
+  end
+
 end
