04793aa0adbe847552c3b72fe480c93314acba4f
mangochiman <mangochiman@gmail.com>
2016-11-15 18:01:17 +0200

    Migrations

--- db/migrate/20161113114724_create_news.rb
+++ db/migrate/20161113114724_create_news.rb
@@ -1,7 +1,9 @@
 class CreateNews < ActiveRecord::Migration
   def self.up
-    create_table :news do |t|
-
+    create_table :news, :primary_key => :news_id do |t|
+      t.string :title
+      t.text :data
+      t.boolean :active, :default => false
       t.timestamps
     end
   end
--- db/migrate/20161113114829_create_updates.rb
+++ db/migrate/20161113114829_create_updates.rb
@@ -1,7 +1,9 @@
 class CreateUpdates < ActiveRecord::Migration
   def self.up
-    create_table :updates do |t|
-
+    create_table :updates, :primary_key => :update_id do |t|
+      t.string :title
+      t.text :details
+      t.boolean :active, :default => false
       t.timestamps
     end
   end
