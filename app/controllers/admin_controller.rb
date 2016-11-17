9b608757d588814a123b91b075b2ea0036c7c78e
mangochiman <mangochiman@gmail.com>
2016-11-17 13:17:06 +0200

    Updates

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -512,7 +512,16 @@ class AdminController < ApplicationController
   end
 
   def show_update
+    primary_category_id = Category.find_by_name("PRIMARY").category_id
+    secondary_category_id = Category.find_by_name("SECONDARY").category_id
+    tertiary_category_id = Category.find_by_name("TERTIARY").category_id
 
+    @primary_books = BookCategory.find(:all, :joins => "INNER JOIN books USING (book_id)", :conditions => ["category_id =?", primary_category_id])
+    @secondary_books = BookCategory.find(:all, :joins => "INNER JOIN books USING (book_id)", :conditions => ["category_id =?", secondary_category_id])
+    @tertiary_books = BookCategory.find(:all, :joins => "INNER JOIN books USING (book_id)", :conditions => ["category_id =?", tertiary_category_id])
+
+    @update = Update.find(params[:update_id])
+    @page_title = @update.title
   end
 
   def remove_updates_menu
